5255cad3706ecbf1d7080ba3e5cba0f9dc7dc9b9
Niki <niittymaki.niki@gmail.com>
2019-02-25 20:41:01 +0200

    Start of the project
    Placeholder text and content area implemented.
    Some color testing.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Loading from './Loading';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component{
+    state = {
+        today: new Date().getDay(),
+        time: new Date().toLocaleTimeString(),
+        lat: null, lng: null,
+        errorMessage: ''
+};
+    
+    componentDidMount(){
+           this.getCoords();
+    }
+    async getCoords(){
+        await window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude, 
+                                        lng: position.coords.longitude}),
+            error =>    this.setState({ errorMessage:error.message })
+        );
+    }
+    renderContent(){
+        // Checks whether there's an error
+        if(this.state.errorMessage && !this.state.lat && !this.state.lng){
+            return <div>
+                <Loading message={this.state.errorMessage}/>
+            </div>
+        }
+        // Checks if the location has been received
+        if(!this.state.errorMessage && this.state.lat && this.state.lng){
+            // this.getWeather(this.state.lat, this.state.lng);
+            return (
+                <div className="container">
+                    <div className="toolbar">
+                        <p>Carbon Dioxide Emissions</p>
+                    </div>
+                    <div className="content">
+                        <p>Nunc sed placerat massa. Etiam ultrices, magna vel faucibus lobortis, lacus dolor fermentum diam, at cursus dui nibh nec erat. In scelerisque vestibulum lorem, id venenatis mi sodales sed. Nullam feugiat libero lorem, non consequat tellus ultrices sed. Nulla quis nunc at orci iaculis molestie a et velit. Nulla sit amet tempus tellus. Suspendisse aliquet eu dui vitae rutrum. Maecenas cursus auctor dolor sed rhoncus. Proin vestibulum dapibus felis, a dignissim nisl mollis auctor. Curabitur diam enim, eleifend sed turpis et, pulvinar mollis dolor. Ut ut lectus rutrum, dictum lectus faucibus, maximus est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque posuere mauris vitae velit scelerisque, cursus rutrum magna tempor. Nullam cursus, metus vel dignissim tempor, sapien augue accumsan elit, ut luctus nulla velit varius nulla.</p>
+                    </div>
+                </div>
+            )
+            
+        }
+        // If neither of the above work:
+        // It renders a "loading" window until the user accepts the location request
+        return <div><Loading message="Please accept the location request.."/></div>
+    }
+    
+    render(){
+        return (
+            
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+    }
+        
+    
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
