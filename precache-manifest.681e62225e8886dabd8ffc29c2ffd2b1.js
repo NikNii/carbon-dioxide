c3f3e4d0a753a27a7da633c7d1b004df80bd4055
Niki <niittymaki.niki@gmail.com>
2019-02-28 16:54:20 +0200

    Updates

---
+++ precache-manifest.681e62225e8886dabd8ffc29c2ffd2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9b870d2f3da786b447",
+    "url": "/NikNii/carbon-dioxide/static/js/runtime~main.8e9b870d.js"
+  },
+  {
+    "revision": "32a921421afcdcdb2ad2",
+    "url": "/NikNii/carbon-dioxide/static/js/main.32a92142.chunk.js"
+  },
+  {
+    "revision": "846fb05947e17ea25d59",
+    "url": "/NikNii/carbon-dioxide/static/js/2.846fb059.chunk.js"
+  },
+  {
+    "revision": "32a921421afcdcdb2ad2",
+    "url": "/NikNii/carbon-dioxide/static/css/main.94956b7f.chunk.css"
+  },
+  {
+    "revision": "bd63d2b73754fb6fbd89d0bf96a1a727",
+    "url": "/NikNii/carbon-dioxide/index.html"
+  }
+];
